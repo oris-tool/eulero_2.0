1a74528cf679636b23f458a0c37f6c387072f1ca
RiccardoYorkeReali <Ricca_Reo_94@hotmail.it>
2023-08-28 16:51:46 +0200

    recent updates

--- eulero/src/main/java/org/oristool/eulero/modeling/activitytypes/ANDType.java
+++ eulero/src/main/java/org/oristool/eulero/modeling/activitytypes/ANDType.java
@@ -15,26 +15,6 @@ public class ANDType extends DAGType{
     public ANDType(ArrayList<Activity> children) {
         super(children);
     }
-    /*@Override
-    public void initActivity(Composite activity, Activity... children){
-        if (children.length == 0)
-            throw new IllegalArgumentException("Parallel cannot be empty");
-
-        double low = 0;
-        double upp = 0;
-        for(Activity act: children){
-            low = Math.max(low, act.low().doubleValue());
-            upp = Math.max(upp, act.upp().doubleValue());
-        }
-
-        initPreconditions(activity, children);
-
-        activity.setMin(activity.low());
-        activity.setMax(activity.upp());
-        activity.setActivities(new ArrayList<>(List.of(children)));
-        activity.setEnumType(ActivityEnumType.AND);
-    }*/
-
     @Override
     public void initPreconditions(Composite activity, Activity... children) {
         for (Activity act : children) {
